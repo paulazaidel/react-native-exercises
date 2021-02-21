f9d2126151dc5840d2c77fa3af6a6ce309251264
Paula Zaidel <paulazaaidel@gmail.com>
2021-02-21 08:40:08 +0000

    Created multi components in one file

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import React from 'react'
 import { View } from 'react-native'
 
 import FirstComponent from './components/FirstComponent'
+import OfficialComponent, { Component1, Component2 } from './components/MultiComponents'
+
 
 export default () => (
     <View>
         <FirstComponent />
+        <OfficialComponent />
+        <Component1 />
+        <Component2 />
     </View>
 )
---
+++ src/components/MultiComponents.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+export default Component = () => <Text> Official Component</Text> 
+
+const Component1 = () => <Text> Component #01</Text>
+
+const Component2 = () => <Text> Component #02</Text>
+
+export { Component1, Component2 }
